1884c2dc1c6c648050a85b26068af26db4230613
aloy33 <fsrshad.bahari.33@gmail.com>
2026-09-07 01:47:20 +0330

    fix: postcss config

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,13 +1,7 @@
-import path from 'path';
-import { defineConfig } from 'prisma/config';
-
-export default defineConfig({
-	earlyAccess: true,
-	schema: path.join('prisma', 'schema.prisma'),
-	migrate: {
-		async adapter() {
-			const { PrismaNeon } = await import('@prisma/adapter-neon');
-			return new PrismaNeon({ connectionString: process.env.DATABASE_URL });
-		},
+const config = {
+	plugins: {
+		'@tailwindcss/postcss': {},
 	},
-});
+};
+
+export default config;
